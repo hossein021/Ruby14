b731094b3560c35f6c38fe93802db7081e35e433
Hossein <hosseinsalmani1373@gmail.com>
2021-06-05 22:05:49 +0430

    if

---
+++ main19.rb
@@ -0,0 +1,38 @@
+# شرط if
+
+# if !true
+#   puts "hello"
+# else
+#   puts "bye"
+# end
+
+condition = false
+if condition
+  puts "Hello"
+else
+  puts "Bye"
+end
+
+another_condition = true
+if condition || another_condition
+  puts "Hi"
+else
+  puts "Bye"
+end
+
+if condition && another_condition
+  puts "Hi"
+else
+  puts "Bye"
+end
+
+name = "Ali"
+if name == "Ali"
+  puts "Welcome Ali"
+elsif name == "Mohammad"
+  puts "Welcome Mohammad"
+elsif name == "Hossein"
+  puts "Welcome Hossein"
+else
+  puts "Welcome To My Program"
+end
